245205994abdfcfce6f7e4c044f3cf347ce83280
Ksenia Sossin <sossin.ksenia@gmail.com>
2023-11-12 18:26:29 +0000

    Create 3 database models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Category(models.Model):
+    """
+    Model acting as categories linked to each Recipe.
+    """
+    recipe_category = models.CharField('Category', max_length=40, blank=False)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['recipe_category']
+
+    def __str__(self):
+        return self.recipe_category
+
+
+class Recipe(models.Model):
+    """
+    This Class creates the Recipe model.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_recipes")
+    updated_on = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
+    cooking_time = models.IntegerField()
+    ingredients = models.TextField()
+    describtion = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    """
+    Model acting as comments linked to each Recipe
+    """
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}" 
+
--- sweetcode/settings.py
+++ sweetcode/settings.py
@@ -88,8 +88,9 @@ WSGI_APPLICATION = 'sweetcode.wsgi.application'
 # }
 
 DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
