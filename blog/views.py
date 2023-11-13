c222bf152b6253aa8a78a8481102805f4ead6b8b
Ksenia Sossin <sossin.ksenia@gmail.com>
2023-11-13 20:33:16 +0000

    Add template html files, static folder, add core for base.html file

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from djanog.views import generic
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
