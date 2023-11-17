5c591bb8a82308d8a6848356189b1d04fb5f1446
Ksenia Sossin <sossin.ksenia@gmail.com>
2023-11-17 19:50:33 +0000

    create a template recipe details and connect up urls in the urls.py file

--- blog/views.py
+++ blog/views.py
@@ -9,16 +9,18 @@ class RecipeList(generic.ListView):
     template_name = 'index.html'
 
 
-class PostDetail(View):
-    def get(self, slug, *args, **kwargs):
+class RecipeDetail(View):
+ 
+    def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        comments = recipe.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
-            liked = True
+            liked = True        
 
         return render(
+            request,
             "recipe_detail.html",
             {
                 "recipe": recipe,
